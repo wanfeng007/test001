83ec5543b1eba9a11c83db4ab818b801ee59f8ce
wanfeng <541092754@qq.com>
2020-10-29 10:29:51 +0800

    第二次提交git

--- test000/src/test000/Test001.java
+++ test000/src/test000/Test001.java
@@ -11,7 +11,7 @@ public class Test001 {
 		List<Map<String, Object>> dataList=new ArrayList<Map<String, Object>>();
         Map<String, Object> map=new HashMap<String, Object>();
         map.put("CUSTOMER_NO","123456");
-        map.put("CUSTOMER_NAME","渤海银行股份杭州分行124");
+        map.put("CUSTOMER_NAME","渤海银行股份杭州分行2020");
         map.put("CUSTOMER_RISK","高风险");
         for(int i=0;i<5;i++) {
 			dataList.add(map);
